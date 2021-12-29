1b70b1cef5ef473da3f4f9f383e7790ab2480c69
sadt <suryaadtmaja13@gmail.com>
2021-12-29 18:08:26 +0700

    add prisma orm

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import autoload from 'fastify-autoload'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+import pkg from '@prisma/client'
+
+const { PrismaClient } = pkg
+
+
+const prisma = new PrismaClient({
+  errorFormat: 'pretty'
+})
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+export default async function (fastify, opts) {
+  const app = fastify
+  app.decorate('prisma', prisma)
+  app.register(autoload, {
+    dir: join(__dirname, 'routes')
+  })
+
+  app.register(autoload, {
+    dir: join(__dirname, 'plugins')
+  })
+
+  return app
+}
